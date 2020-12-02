54bafad41d1975eab6a8732d044fc8e691223ab3
Adrii98 <adrian.blanco01@estudiant.upf.edu>
2020-12-02 15:18:11 +0100

    prueba1

--- Practica2/Exercise1.py
+++ Practica2/Exercise1.py
@@ -5,15 +5,24 @@ def compute(video_name):
     os.system("ffmpeg -i " + video_name + " 2> output.txt")
 
 
+def rename(file):
+    new_name = 'BBB_renamed.mp4'
+    os.rename(file, new_name)
+
+
+def change_codec(video):
+    act = "ffmpeg -i " + video + "-acodec mp3 -vcodec copy output_codec_changed.mp4"
+    os.system(act)
+
+
 def main():
     compute("bbb.mp4")
     with open('output.txt', 'r') as file:
         for line in file:
-            if (line.strip(" ").startswith("Duration") or line.strip(" ").startswith("title") or line.strip(" ").startswith("artist") or
+            if (line.strip(" ").startswith("Duration") or line.strip(" ").startswith("title") or line.strip(
+                    " ").startswith("artist") or
                     line.strip(" ").startswith("comment")):
-            #if line.strip(" ").startswith("title"):
                 print(line)
-
 
 
 main()
